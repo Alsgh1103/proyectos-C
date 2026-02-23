bdd52d248a015778bbc8bc5740761344ed36534a
Alexis <alogh11032002@gmail.com>
2026-02-23 09:55:22 -0500

    BinaryTree agregado

---
+++ BinaryTree/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "util.h"
+#include "pointers.h"
+#include "containers/array.h"
+#include "containers/lists.h"
+#include "DemoBinaryTree.cpp"
+#include "DemoAVL.cpp"
+
+// g++ -std=c++17 -Wall -g -pthread main.cpp -o main
+
+int main() {
+    std::cout << "Hello EDA-UNMSM!" << std::endl;
+    // DemoUtil();
+    // DemoPointers1();
+    // DemoArray();
+    // DemoLists();
+    DemoBinaryTree();
+    DemoAVL();
+    
+    return 0;
+}
